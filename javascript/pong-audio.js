9ff5d75b4d22084020d63f96b22bf3a2bd927137
jazzsnz04 <jasminesaenz99@gmail.com>
2025-12-23 19:30:54 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -46,7 +46,7 @@ export var soundArray = []; //list of sounds loaded
 export var wallSound = new soundFile("silence.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
-export var paddleSound = new soundFile("shimmer.mp3");
+export var paddleSound = new soundFile("click.mp3");
 soundArray.push(paddleSound);
 
 export var scoreSound = new soundFile("silence.mp3");
